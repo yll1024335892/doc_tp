1b0f4e2ffb1ebb32e0a47bba829d6aa15f365c03
yll1024335892 <yll1024335892@163.com>
2019-02-03 12:27:22 +0800

    添加添加首页显示的内容

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -14,27 +14,37 @@ class Index extends Controller
      */
     public function index()
     {
-      //  $list=  Db::table('ayi_project_category')->alias('a')->join('__PROJECT__ b','a.id = b.type_id')->limit(0,8)->select();
-        $cateModel=new ProjectCategoryModel();
-        $cateList=$cateModel->select();
-        $projectModel=new ProjectModel();
-        foreach ($cateList as $key=>$val){
-            $cateList[$key]['list']=$projectModel->where("type_id","eq",$val['id'])->select();
+        $projectModel = new ProjectModel();
+        $listData = $projectModel->where("is_home", "eq", "1")->limit(0, 30)->select();
+        return $this->fetch("", ['list' => $listData]);
+    }
+
+    public function cateList()
+    {
+        $cateModel = new ProjectCategoryModel();
+        $cateList = $cateModel->where("parent_id", "eq", "0")->select();
+        $projectModel = new ProjectModel();
+        foreach ($cateList as $key => $val) {
+            $cateList[$key]['list'] = $projectModel->where("type_id", "eq", $val['id'])->limit(0, 8)->select();
         }
-        return $this->fetch("",['cateList'=>$cateList]);
+        return $this->fetch("", ['cateList' => $cateList]);
     }
 
     /**
      * 文档的列表页面显示
      */
-    public function doclist(){
-        return $this->fetch();
+    public function doclist()
+    {
+        $cateModel = new ProjectCategoryModel();
+        $cateList = $cateModel->where("parent_id", "neq", "0")->where("is_show","eq","1")->select();
+        return $this->fetch("",['cate'=>$cateList]);
     }
 
     /**
      * 文档的具体的详细
      */
-    public function  docdetail(){
+    public function docdetail()
+    {
         return $this->fetch();
     }
 
